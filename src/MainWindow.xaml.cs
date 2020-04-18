d59fd4ab413a310b189f4b0dbf7cadacb97db472
Angshuman <angshuman.agarwal@gmail.com>
2020-04-18 21:57:18 +0100

    Code check-in

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Windows;
+using CefSharp;
+
+namespace CefSharp_Video_Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Browser.Loaded += Browser_Loaded;
+        }
+
+        private void Browser_Loaded(object sender, RoutedEventArgs e)
+        {
+            Browser.LoadHtml(File.ReadAllText(@"file:///C:/Users/jimmy/source/repos/CefSharp_Video_Sample/bin/Debug/Video.html"));
+        }
+    }
+}
